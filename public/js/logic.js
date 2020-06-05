1ad112f1f86deee1e97a42caaf659ec6e6bd47f2
Thorn12 <cjtyacosta99@hotmail.com>
2020-06-05 20:53:11 +0200

    First changes of the rijksmuseum-app

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,14 +1,23 @@
-<<<<<<< HEAD
 $('#sendMsgBtn').click(exampleAlert); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
 
 function exampleAlert() {
 	alert("Author: " + $('#author').val() + "\nMessage: " + $('#message').val());
 	console.log("Button pressed!");
-=======
-$('#sendMsgBtn').click(exampleAlert); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
+}
 
-function exampleAlert() {
-	alert("Author: " + $('#author').val() + "\nMessage: " + $('#message').val());
-	console.log("Button pressed!");
->>>>>>> 6e1d5bfeb9baa22c648d675966742ca805bee05f
+function filterFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
