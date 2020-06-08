3b150422d656b20fce647e388b9ddb8909a4cdc5
Explorder <Explorder@users.noreply.github.com>
2020-06-08 15:56:18 +0200

    API Link

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,8 +1,7 @@
-$('#sendMsgBtn').click(exampleAlert); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
+$('#stealBtn').click(exampleAlert); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
 
 function exampleAlert() {
-	alert("Author: " + $('#author').val() + "\nMessage: " + $('#message').val());
-	console.log("Button pressed!");
+	console.log("Nice");
 }
 
 function filterFunction() {
@@ -19,5 +18,37 @@ function filterFunction() {
         } else {
             li[i].style.display = "none";
         }
+    }
+}
+// The above function is from w3c gxOqGef4
+
+$(document).ready(function() {
+   getPainting();
+
+})
+function getPainting() {
+    var paitingsFound = true;
+    var pageNumber = Math.floor((Math.random() * 792));
+    console.log(pageNumber);
+    $.get("https://www.rijksmuseum.nl/api/nl/collection?key=gxOqGef4&type=schilderij&ps=6&p=" + pageNumber, function(data) {
+        console.log(data);
+        for(let i = 0; i < 6; i++) {
+            if(data.artObjects[i].hasImage == false) {
+                paitingsFound = false;
+                getPainting();
+                break;
+            }
+        }
+        if(paitingsFound == true){
+            placePainting(data)
+        }    
+    })
+}
+//The above fuction generates a group of 6 paitings that all have an img link.
+//page withouit images 593.
+
+function placePainting(data){
+    for(let i = 0; i < 6; i++) {
+        document.getElementById("image" + i).src = data.artObjects[i].webImage.url;
     }
 }
