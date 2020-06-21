63d54c7fce864d503ac28443a738cb344e8bb511
Explorder <Explorder@users.noreply.github.com>
2020-06-21 22:19:29 +0200

    Nearly Finished

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,6 +1,6 @@
 //Global Variables
-var Gallerij;
-var indiPaitings = [];
+var sixPaintings;
+var individualPaitings = [];
 var stolenPaitings = []; 
 
 //When webpage is opened
@@ -13,21 +13,22 @@ $('#nextHall').click(getPainting);
 $('#stealBtn').click(steal);
 $('#leaveBtn').click(leave);
 
+
 function steal() {
     for(let i = 0; i < 6; i++){
         if (document.getElementById("box" + i).checked == true) {
             for(let j = 0; j < stolenPaitings.length; j++){
-                if((Gallerij.artObjects[i].title + "\n") == stolenPaitings[j]) {
+                if((sixPaintings.artObjects[i].title + "\n") == stolenPaitings[j]) {
                     alert("You are trying to steal a painting you have already stolen")
                     return;
                 }
             }
-            if(check() == true) {
+            if(check(i) == true) {
                 var li = document.createElement("li");
-                var namePainting = document.createTextNode(Gallerij.artObjects[i].title);
+                var namePainting = document.createTextNode(sixPaintings.artObjects[i].title);
                 li.appendChild(namePainting)
                 document.getElementById("takenList").appendChild(li);
-                stolenPaitings.push(Gallerij.artObjects[i].title + "\n");
+                stolenPaitings.push(sixPaintings.artObjects[i].title + "\n");
             }
             else {
                 alert("You got caught\nYou have to start again");
@@ -39,11 +40,33 @@ function steal() {
     }
 }
 
-function check() {
-    var check = Math.random()* 100
-    console.log(check)
-    var  modifier;
-    if(check > 80) {
+function check(i) {
+    let check = Math.random()* 100
+    let modifier = 0;
+    if(i != 7) {
+        let surface = parseFloat(individualPaitings[i].artObject.dimensions[0].value) * parseFloat(individualPaitings[i].artObject.dimensions[1].value);
+        if (surface < 2500) {
+            modifier = 1;
+        }
+        else if (surface < 4900) {
+            modifier = 2;
+        }
+        else if (surface < 8100) {
+            modifier = 4;
+        }
+        else if (surface < 14400) {
+            modifier = 6;
+        }
+        else if (surface < 22500) {
+            modifier = 8;
+        }
+        else {
+            modifier = 10;
+        }
+    }
+    modifier += stolenPaitings.length;
+    console.log(check + modifier);
+    if(check + modifier > 90) {
         return false;
     }
     else {
@@ -53,84 +76,35 @@ function check() {
 
 
 function leave() {
-    if(check() == false) {
+    if(check(7) == false) {
         alert("You got caught on your way out \nYou have to start over again")
         document.getElementById("takenList").innerHTML = "";
         stolenPaitings = [];
     }
     else {
         sPaintings = stolenPaitings.toString().replace(",", "");
+        console.log(sPaintings)
         alert("The paintings that you got away with are: \n" + sPaintings);
         document.getElementById("takenList").innerHTML = "";
         stolenPaitings = [];
     }
-    // for(let i = 0; i < 6; i++) {
-    //     //console.log(Gallerij)
-    //     $.get("https://www.rijksmuseum.nl/api/nl/collection/" + Gallerij.artObjects[i].objectNumber + "?key=gxOqGef4", function(schilderij) {
-    //         console.log("Hoogte: " + schilderij.artObject.dimensions[0].value)
-    //         console.log("Breedte: " + schilderij.artObject.dimensions[1].value)
-    //     })
-    // }
-    // //alert(" You have left the museum")
 }
 
-function filterFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-// The above function is from w3c gxOqGef4
-
-
+// Code = gxOqGef4
 
 function getPainting() {
-    var paitingsFound = true;
+    individualPaitings = [];
+    sixPaintings = [];
     var pageNumber = Math.floor((Math.random() * 792));
     console.log(pageNumber);
-    $.get("https://www.rijksmuseum.nl/api/nl/collection?key=gxOqGef4&type=schilderij&ps=6&p=" + pageNumber , function(data) {
-    //$.get("https://www.rijksmuseum.nl/api/nl/collection?key=gxOqGef4&type=schilderij&ps=6&p=358", function(data) {
-        console.log(data);
-        for(let i = 0; i < 6; i++) {
-            if(data.artObjects[i].hasImage == false) {
-                getPainting();
-                return;
-            }
-        }
-            Gallerij = data;
-            // indiPaitings = [];
-            // for(let i = 0; i < 6; i++) {
-            //     $.get("https://www.rijksmuseum.nl/api/nl/collection/" + Gallerij.artObjects[i].objectNumber + "?key=gxOqGef4", function(schilderij) {
-            //         console.log(schilderij)
-            //         indiPaitings.push(schilderij);
-            //     })
-            // }
-            // console.log("Hoogte: " + indiPaitings[3].artObject.dimensions[0].value)
-            // console.log("Breedte: " + indiPaitings[3].artObject.dimensions[1].value) 
-            placePainting()   
-    })
-
+    getSixPaintings("https://www.rijksmuseum.nl/api/nl/collection?key=gxOqGef4&type=schilderij&ps=6&p=" + pageNumber);
 }
-//The above fuction generates a group of 6 paitings that all have an img link.
-// //page withouit images 593.
-// console.log("Hoogte: " + indiPaitings[i].artObject.dimensions[0].value)
-// console.log("Breedte: " + indiPaitings[i].artObject.dimensions[1].value)
 
 function placePainting(){
     for(let i = 0; i < 6; i++) {
-        document.getElementById("image" + i).src = Gallerij.artObjects[i].webImage.url; // Changes the paiting(image)
-        document.getElementById("tekst" + i).innerHTML = Gallerij.artObjects[i].title; // Changes the name
-        document.getElementById("boxTekst" + i).innerHTML = Gallerij.artObjects[i].title;
+        document.getElementById("image" + i).src = sixPaintings.artObjects[i].webImage.url; // Changes the paiting(image)
+        document.getElementById("tekst" + i).innerHTML = sixPaintings.artObjects[i].title; // Changes the name
+        document.getElementById("boxTekst" + i).innerHTML = sixPaintings.artObjects[i].title;
     }
 }
 for(let i = 0; i < 6; i++) {
@@ -144,8 +118,17 @@ for(let i = 0; i < 6; i++) {
     img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
-    captionText.innerHTML = this.alt ;
+    let dimensions = individualPaitings[i].artObject.subTitle.replace("h", "Hoogte").replace("d", "Dikte").replace("b","Breedte");
+    captionText.innerHTML = "<ul> <li> Name: " +  individualPaitings[i].artObject.title + "</li>" +
+                            "<li> ID:  " + individualPaitings[i].artObject.id + "</li>" +
+                            "<li> Maker:  " + individualPaitings[i].artObject.principalMaker + "</li>" +
+                            "<li> Medium:  " + individualPaitings[i].artObject.physicalMedium + "</li>" +
+                            "<li> Presenting Date:  " + individualPaitings[i].artObject.dating.presentingDate + "</li>" +
+                            "<li> Description:  " + individualPaitings[i].artObject.description + "</li>" +
+                            "<li> Dimensions of the painting:  " + dimensions + "</li></ul>";
+    captionText.style.color = 'white';
     }
+
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[i];
@@ -154,6 +137,59 @@ for(let i = 0; i < 6; i++) {
     span.onclick = function() { 
     modal.style.display = "none";
     }
+}
+
+function getSixPaintings(url) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url, true);
+
+	xmlhttp.send();
+	xmlhttp.addEventListener("readystatechange", processRequest, false);
+
+	function processRequest(e) {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            json_file = JSON.parse(xmlhttp.responseText);
+            for(let i = 0; i < 6; i++) {
+                if(json_file.artObjects[i].hasImage == false) {
+                    getPainting();
+                    return;
+                }
+            }
+            sixPaintings = json_file;
+            getIndividualPaintings(0);
+		}
+	}
+}
+
+function getIndividualPaintings(number) {
+    var url = "https://www.rijksmuseum.nl/api/nl/collection/" + sixPaintings.artObjects[number].objectNumber + "?key=gxOqGef4"
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, true);
+
+    xmlhttp.send();
+    xmlhttp.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(e) {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            json_file = JSON.parse(xmlhttp.responseText);
+            individualPaitings.push(json_file);
+            if (number < 5) {
+                number++
+                getIndividualPaintings(number)
+            }
+            else {
+                test2()
+                placePainting();
+            }
+        }
+    }
+}
+
+function Test() {
+    console.log("test")
+}
+function test2() {
+    console.log(individualPaitings)
 }
 
 // The code makes uses of modals to show certain hidden information it iterates through the whole list of images to enable the functionality of a modal to every image.
